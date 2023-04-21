9aae6f39fb877829099671c03fc68e6293c64bb6
Douglas Lima <douglas07a@gmail.com>
2023-04-21 10:28:44 -0300

    Comando Swiper

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView:3,
+    spaceBetween: 10,
+    slidePerGroup:3,
+    pagination: {
+      el: '.swiper-pagination',
+    }, 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
 function clickMenu() {
     if(menu.style.display == 'block') {
         menu.style.display = 'none'
@@ -5,3 +25,4 @@ function clickMenu() {
         menu.style.display = 'block'
     }
 }
+
