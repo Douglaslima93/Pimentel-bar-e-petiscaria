d03a225314ef81675f7cf9dd1c881c157bbbf248
Douglas Lima <douglas07a@gmail.com>
2023-04-24 13:21:43 -0300

    Carrossel de imagens

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-/*
-const swiper = new Swiper('.swiper', {
-    
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView:3,
-    spaceBetween: 10,
-    slidePerGroup:3,
-    pagination: {
-      el: '.swiper-pagination',
-    }, 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-
 function clickMenu() {
     if(menu.style.display == 'block') {
         menu.style.display = 'none'
@@ -47,4 +5,3 @@ function clickMenu() {
         menu.style.display = 'block'
     }
 }
-*/
